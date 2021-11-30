a85a9116e021e3f52359fd5cc122ded8252f0177
andmansim <91722324+andmansim@users.noreply.github.com>
2021-11-30 12:54:42 +0100

    Definición de constantes

--- andrea.py
+++ andrea.py
@@ -1,3 +1,11 @@
 #Importación de módulos
 import pygame
 from pygame.locals import *
+
+#Definición de constantes
+
+# Constantes para la inicialización de la superficie de dibujo
+VENTANA_HORI = 800  # Ancho de la ventana
+VENTANA_VERT = 600  # Alto de la ventana
+FPS = 60  # Fotogramas por segundo
+BLANCO = (255, 255, 255)  # Color del fondo de la ventana (RGB)
