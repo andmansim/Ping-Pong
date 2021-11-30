5f5d1f68f99aeec24dcefd68c140f1db1c6f45bf
andmansim <91722324+andmansim@users.noreply.github.com>
2021-11-30 22:52:54 +0100

    terminado

---
+++ Paso2(Andrea).py
@@ -0,0 +1,83 @@
+# Paso 2: Clase de PelotaPong
+
+#Importación de módulos
+import random
+#Importación de módulos
+import pygame
+
+#Importación de módulos
+import random
+from pygame.locals import QUIT
+
+#Definición de constantes
+
+# Constantes para la inicialización de la superficie de dibujo
+VENTANA_HORI = 800  # Ancho de la ventana
+VENTANA_VERT = 600  # Alto de la ventana
+FPS = 60  # Fotogramas por segundo
+BLANCO = (255, 255, 255)  # Color del fondo de la ventana (RGB)
+#Clase PelotaPong
+class PelotaPong:
+    def __init__(self, fichero_imagen):
+        # --- Atributos de la Clase ---
+
+        # Imagen de la Pelota
+        self.imagen = pygame.image.load(fichero_imagen).convert_alpha() 
+
+        # Dimensiones de la Pelota
+        self.ancho, self.alto = self.imagen.get_size()
+
+        # Posición de la Pelota
+        self.x = VENTANA_HORI / 2 - self.ancho / 2
+        self.y = VENTANA_VERT / 2 - self.alto / 2
+
+        # Dirección de movimiento de la Pelota
+        self.dir_x = random.choice([-5, 5])
+        self.dir_y = random.choice([-5, 5])
+
+    def mover(self):
+        self.x += self.dir_x
+        self.y += self.dir_y
+    def rebotar(self):
+        
+        if self.x <= -self.ancho:
+            self.reiniciar()    
+        if self.x >= VENTANA_HORI:
+            self.reiniciar()
+        if self.y <= 0:
+            self.dir_y = -self.dir_y
+        if self.y + self.alto >= VENTANA_VERT:
+            self.dir_y = -self.dir_y
+    def reiniciar(self):
+        self.x = VENTANA_HORI / 2 - self.ancho / 2
+        self.y = VENTANA_VERT / 2 - self.alto / 2
+        self.dir_x = -self.dir_x
+        self.dir_y = random.choice([-5, 5])
+                    
+#Inicialización
+def main():
+    pygame.init()
+    # Inicialización de la superficie de dibujo (display surface)
+    ventana = pygame.display.set_mode((VENTANA_HORI, VENTANA_VERT))
+    pygame.display.set_caption("Pong 2")
+    
+    #Creación de la pelota
+    pelota = PelotaPong("bola_roja.png") 
+    
+    # Bucle principal
+    jugando = True
+    while jugando:
+        #Mover la pelota
+        pelota.mover()
+        pelota.rebotar()
+        ventana.fill(BLANCO)
+        ventana.blit(pelota.imagen, (pelota.x, pelota.y))
+
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                jugando = False
+
+        pygame.display.flip()
+        pygame.time.Clock().tick(FPS)
+
+    pygame.quit()
