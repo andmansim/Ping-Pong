d00e2f29e706e20217c4e12cddc41df911b436ea
andmansim <91722324+andmansim@users.noreply.github.com>
2021-11-30 19:44:51 +0100

    Modificar la puntuación

--- Paso9.py
+++ Paso9.py
@@ -4,3 +4,12 @@
 # Puntuación de la pelota
 self.puntuacion = 0
 self.puntuacion_ia = 0
+
+#Modificar la puntuación
+def rebotar(self):
+    if self.x <= -self.ancho:
+        self.reiniciar()
+        self.puntuacion_ia += 1
+    if self.x >= VENTANA_HORI:
+        self.reiniciar()
+        self.puntuacion += 1
